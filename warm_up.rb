3c74175dc7e43ddead80fbcacda415b28f6042ba
cjvirtucio87 <cjv287@gmail.com>
2016-07-15 10:26:34 -0700

    Finished parse_tag warmup

--- warm_up.rb
+++ warm_up.rb
@@ -3,13 +3,17 @@ def parse_tag(string)
   /\A\<(\w)/.match(string)
   tag[:type] = $1
 
-  /\bclass=\'(\w+)\'/.match(string)
-  tag[:class] = $1
+  /class=['"]([\w\s\-]+)['"]/.match(string)
+  tag[:class] = $1.split(' ')
 
-  
+  /id=['"]([\w\s\-]+)['"]/.match(string)
+  tag[:id] = $1
+
   tag
 end
 
 
 tag = parse_tag("<p class='foo bar' id='baz'>")
-p tag[:class]
+tag.each do |pair|
+  p "#{pair[0]}: #{pair[1]}"
+end
