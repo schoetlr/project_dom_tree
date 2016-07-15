97509fa7480b5627f7e61f48314b691ca46de019
schoetlr <srlrs20020@gmail.com>
2016-07-15 14:01:27 -0400

    working on parse_html

--- warm_up.rb
+++ warm_up.rb
@@ -12,17 +12,22 @@ HTML_STRING =
 
 def parse_tag(string)
   tag = {}
-  /\A\<(\w)/.match(string)
+  /\A\<(\w+)/.match(string)
   tag[:type] = $1
 
-  /class=['"]([\w\s\-]+)['"]/.match(string)
-  tag[:class] = $1.split(' ')
+  if /class=['"]([\w\s\-]+)['"]/.match(string)
+    tag[:class] = $1.split(' ')
+  end
 
-  /id=['"]([\w\s\-]+)['"]/.match(string)
-  tag[:id] = $1
+  if
+    /id=['"]([\w\s\-]+)['"]/.match(string)
+    tag[:id] = $1
+  end
 
-  /name=['"]([\w\s\-]+)['"]/.match(string)
-  tag[:name] = $1
+  if 
+    /name=['"]([\w\s\-]+)['"]/.match(string)
+    tag[:name] = $1
+  end
 
   tag
 end
@@ -30,13 +35,25 @@ end
 
 def parse_html(string)
   html = {}
-  string.split(' ')
+  tags = string.scan(/(<.+>)/)
+  text_nodes = string.scan(/>(.+)</)
+  tags
+  text_nodes
+  # p $1
+  # i = 1
+  # array = []
+  # begin
+  #   array << parse_tag($i)
+  #   i += 1
+  # end until $i.nil?
+  # array
 end
 
+p parse_html(HTML_STRING)
 
 
-
-tag = parse_tag("<p class='foo bar' id='baz'> name='hello'")
-tag.each do |pair|
-  p "#{pair[0]}: #{pair[1]}"
-end
+#test for parse_tag
+# tag = parse_tag("<p class='foo bar' id='baz'> name='hello'")
+# tag.each do |pair|
+#   p "#{pair[0]}: #{pair[1]}"
+# end
